e1ceb779b06aafd46cd77f9cbc740146a6dc45c6
ZL Liu <zlliu@ZLs-MacBook-Pro.local>
2020-09-11 07:47:05 -0700

    initial version

---
+++ README.md
@@ -0,0 +1,46 @@
+# Generalized MTurk Task Template
+A general-purpose template for Amazon Mechanical Turk tasks.
+
+### Development
+Run `make watch` to watch SCSS files and compile to CSS. Run something like `python -m SimpleHTTPServer` to see the page locally.
+
+### Customization
+This framework can be used to create MTurk HITs, broken up into discrete repeated subtasks.
+
+To define your own MTurk HIT, you only need to change things in three places:
+
+#### `config.json`
+Here, you can define your HIT's name, description, number of subtasks, instructions, etc.
+
+Fields:
+* `meta.title` - the title of your task, displayed in the page title (string)
+* `meta.description` - a short overview of your task's purpose, displayed as bolded text right below the page title (string)
+* `meta.numSubtasks` - the number of subtasks your task will have (int)
+* `meta.disclaimer` - the experiment disclaimer text displayed at the bottom of the page (string)
+* `meta.aggregate` - whether inputs and outputs for the task should be divided up by subtasks, or the same inputs and outputs should be stored across all subtasks. If false, outputs will be stored in an array of length `numSubtasks`; otherwise, they will be merged a single object. In general, setting this to `false` will be better if subtasks are repetitive and self-contained (for example, labeling a series of images), and this to `true` will be better if the behavior of one subtask depends on input/output from another subtask (for instance, labeling an image in one subtask and writing a description of it in the next). 
+* `instructions.simple` - short instruction paragraph displayed below the task description. You can include HTML tags here! (string)
+* `instructions.steps` - an array of instruction strings, displayed as a bulleted list on the page. You can include HTML tags here! (array of strings)
+* `instructions.images` - an array of URLs for demo gifs on the instruction page. One of these will be displayed randomly on each page load. (array of strings)
+* `hitCreation.title` - the HIT's title on the MTurk worker page (string)
+* `hitCreation.description` - the HIT's description on the MTurk worker page (string)
+* `hitCreation.numTasks` - how many duplicates of the HIT you want to create (int)
+* `hitCreation.numAssignments` - how many assignments each duplicated HIT has (int)
+* `hitCreation.rewardAmount` - amount to pay per HIT (string)
+* `hitCreation.keywords` - comma-separated tags (string)
+* `hitCreation.duration` - how long you want to give each worker to complete the task once started in seconds (int)
+* `hitCreation.lifetime` - how long you want the HIT on the worker site in seconds (int)
+* `hitCreation.taskUrl` - the URL where your MTurk task lives (string)
+* `hitCreation.production` - whether you want to use MTurk's production or sandbox environment (bool)
+
+#### `index.html`
+Find the section marked `<!-- vv CUSTOM EXPERIMENT MARKUP GOES HERE vv -->`, and add your custom HTML elements in that section (e.g. image divs, input boxes). Add `id`s to those HTML elements so you can easily refer to them with JQuery in the page's JavaScript (see `assets/js/custom.js`).
+
+#### `assets/js/custom.js`
+Fill out the 4 functions: `loadTasks`, `showTask`, `collectData`, and `validateTask`. These define behavior for loading initial data, displaying a task, storing data from a task, and validating a task. We recommmend starting by copying one of our template files: `assets/js/aggregate-template.js` if you have set `config.meta.aggregate=true` or `assets/js/no-aggregate-template.js` if you have set `config.meta.aggregate=false`. 
+
+### Setting up the MTurk task
+Edit the `hitCreation` fields in `config.json`. Use `script/create_hit.py` to create a HIT. Make sure to edit the config for submitting to live or sandbox.
+
+### Screenshots
+Here's a demo of the task interface:
+![demo](demo.gif)
---
+++ assets/js/main.js
@@ -0,0 +1,221 @@
+const MTURK_SUBMIT = "https://www.mturk.com/mturk/externalSubmit";
+const SANDBOX_SUBMIT = "https://workersandbox.mturk.com/mturk/externalSubmit";
+
+var config = {};
+
+var state = {
+    taskIndex: 0,
+    taskInputs: {}, 
+    taskOutputs: [],
+    assignmentId: gup("assignmentId"),
+    workerId: gup("workerId"),
+};
+
+/* HELPERS */
+function saveTaskData() {
+    console.log(state.taskOutputs);
+    if (config.meta.aggregate) {
+        var updates = custom.collectData(getTaskInputs(state.taskIndex), state.taskIndex, getTaskOutputs(state.taskIndex));
+        $.extend(state.taskOutputs, updates);
+    } else {
+        state.taskOutputs[state.taskIndex] = custom.collectData(getTaskInputs(state.taskIndex), state.taskIndex, getTaskOutputs(state.taskIndex));
+    }
+}
+
+function getTaskInputs(i) {
+    return config.meta.aggregate ? state.taskInputs : state.taskInputs[i];
+}
+
+function getTaskOutputs(i) {
+    return config.meta.aggregate ? state.taskOutputs : state.taskOutputs[i];
+}
+
+function updateTask() {
+    custom.showTask(getTaskInputs(state.taskIndex), state.taskIndex, getTaskOutputs(state.taskIndex));
+    $("#progress-bar").progress("set progress", state.taskIndex + 1);
+    if (state.taskIndex == config.meta.numSubtasks - 1) {
+        $("#next-button").addClass("disabled");
+        if (state.taskIndex != 0) {
+            $("#prev-button").removeClass("disabled");
+        } else {
+            $("#prev-button").addClass("disabled");
+        }
+        $("#submit-button").removeClass("disabled");
+        $("#final-task-fields").css("display", "block");
+    } else if (state.taskIndex == 0) {
+        $("#next-button").removeClass("disabled");
+        $("#prev-button").addClass("disabled");
+        $("#submit-button").addClass("disabled");
+        $("#final-task-fields").css("display", "none");
+    } else {
+        $("#next-button").removeClass("disabled");
+        $("#prev-button").removeClass("disabled");
+        $("#submit-button").addClass("disabled");
+        $("#final-task-fields").css("display", "none");
+    }
+}
+
+function nextTask() {
+    if (state.taskIndex < config.meta.numSubtasks - 1) {
+        saveTaskData();
+        var err = custom.validateTask(getTaskInputs(state.taskIndex), state.taskIndex, getTaskOutputs(state.taskIndex));
+        if (err) {
+            generateMessage("negative", err);
+        } else {
+            state.taskIndex++;
+            updateTask();
+            clearMessage();
+            console.log("Current collected data", state.taskOutputs);
+        }
+    }
+}
+
+function prevTask() {
+    if (state.taskIndex > 0) {
+        saveTaskData();
+        state.taskIndex--;
+        updateTask();
+    }
+}
+
+function toggleInstructions() {
+    if ($("#experiment").css("display") == "none") {
+        $("#experiment").css("display", "flex");
+        $("#instructions").css("display", "none");
+
+        $("#sample-task2").css("display", "block");
+        if ($('#sample-task2').find('img').length) {
+
+        }else{
+            let instructionsIndex = 1;
+            let imgEle = "<img class='instructions-img' src='";
+            imgEle += config.instructions.images[instructionsIndex] + "'></img>";
+            $("#instructions-demo2").append($(imgEle));
+        }
+
+        updateTask();
+    } else {
+        saveTaskData();
+        $("#experiment").css("display", "none");
+        $("#instructions").css("display", "flex");
+        $("#sample-task2").css("display", "none");
+    }
+}
+
+function clearMessage() {
+    $("#message-field").html("");
+}
+
+function generateMessage(cls, header) {
+    clearMessage();
+    var messageStr = "<div class='ui message " + cls + "'>";
+    messageStr += "<i class='close icon'></i>";
+    messageStr += "<div class='header'>" + header + "</div></div>";
+
+    var newMessage = $(messageStr);
+    $("#message-field").append(newMessage);
+    newMessage.click(function() {
+        $(this).closest(".message").transition("fade");
+    });
+}
+
+function addHiddenField(form, name, value) {
+    // form is a jQuery object, name and value are strings
+    var input = $("<input type='hidden' name='" + name + "' value=''>");
+    input.val(value);
+    form.append(input);
+}
+
+function submitHIT() {
+    var submitUrl = config.hitCreation.production ? MTURK_SUBMIT : SANDBOX_SUBMIT;
+    saveTaskData();
+    clearMessage();
+    $("#submit-button").addClass("loading");
+    var form = $("#submit-form");
+    for (var i = 0; i < config.meta.numSubtasks; i++) {
+        var err = custom.validateTask(getTaskInputs(i), i, getTaskOutputs(i));
+        if (err) {
+            $("#submit-button").removeClass("loading");
+            generateMessage("negative", err);
+            return;
+        }
+    }
+
+    addHiddenField(form, 'assignmentId', state.assignmentId);
+    addHiddenField(form, 'workerId', state.workerId);
+    var results = {
+        'inputs': state.taskInputs,
+        'outputs': state.taskOutputs
+    };
+    addHiddenField(form, 'results', JSON.stringify(results));
+    addHiddenField(form, 'feedback', $("#feedback-input").val());
+
+    $("#submit-form").attr("action", submitUrl); 
+    $("#submit-form").attr("method", "POST"); 
+    $("#submit-form").submit();
+    $("#submit-button").removeClass("loading");
+    generateMessage("positive", "Thanks! Your task was submitted successfully.");
+    $("#submit-button").addClass("disabled");
+}
+
+function gup(name) {
+    console.log(name);
+    var regexS = "[\\?&]"+name+"=([^&#]*)";
+    console.log(regexS);
+    var regex = new RegExp( regexS );
+    var tmpURL = window.location.href;
+    console.log(tmpURL);
+    var results = regex.exec( tmpURL );
+    if (results == null) return "";
+    else return results[1];
+}
+
+/* SETUP FUNCTIONS */
+function populateMetadata(config) {
+    $(".meta-title").html(config.meta.title);
+    $(".meta-desc").html(config.meta.description);
+    $(".instructions-simple").html(config.instructions.simple);
+    for (var i = 0; i < config.instructions.steps.length; i++) {
+        $(".instructions-steps").append($("<li>" + config.instructions.steps[i] + "</li>"));
+    }
+    $(".disclaimer").html(config.meta.disclaimer);
+    if (config.instructions.images.length > 0) {
+        $("#sample-task").css("display", "block");
+        var instructionsIndex = Math.floor(Math.random() * config.instructions.images.length);
+        instructionsIndex = 1;
+        var imgEle = "<img class='instructions-img' src='";
+        imgEle += config.instructions.images[instructionsIndex] + "'></img>";
+        $("#instructions-demo").append($(imgEle));
+
+    }
+    $("#progress-bar").progress({
+        total: config.meta.numSubtasks,
+    });
+}
+
+function setupButtons() {
+    $("#next-button").click(nextTask);
+    $("#prev-button").click(prevTask);
+    $(".instruction-button").click(toggleInstructions);
+    $("#submit-button").click(submitHIT);
+    if (state.assignmentId == "ASSIGNMENT_ID_NOT_AVAILABLE") {
+        $("#submit-button").remove();
+    }
+}
+
+/* MAIN */
+$(document).ready(function() {
+    $.getJSON("config.json").done(function(data) {
+        config = data;
+        if (config.meta.aggregate) {
+            state.taskOutputs = {};
+        }
+        custom.loadTasks(config.meta.numSubtasks).done(function(taskInputs) {
+            state.taskInputs = taskInputs;
+            console.log(taskInputs);
+            populateMetadata(config);
+            setupButtons(config);
+        });
+    });
+});
+
